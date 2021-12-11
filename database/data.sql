34d4e1e76c65ade022bf1ff48bf578cb4975f403
Nelson Mello <nelson.mello@sou.unaerp.edu.br>
2021-12-11 19:32:49 -0300

    pronto

---
+++ database/data.sql
@@ -0,0 +1,7 @@
+
+CREATE DATABASE finalapi;
+
+use finalapi;
+
+
+CREATE TABLE finalapi.rick (id int not null AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(45), vivo VARCHAR(45), gender VARCHAR(45), species VARCHAR(45), imagens LONGBLOB);
